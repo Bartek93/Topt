ba7ddc0ca93c33f7d2087343422de6d5b3695c47
Bartosz Krok <bartosz.krok@pro-ids.com>
2017-03-04 17:41:31 +0100

    combobox

--- src/topt/Main.java
+++ src/topt/Main.java
@@ -1,37 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package topt;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Bartolio
- */
 public class Main extends Application {
-	
+
 	@Override
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+		stage.setTitle("TOPT Projekt");
+		stage.setScene(new Scene(root));
+        stage.setResizable(false);
 		
-		Scene scene = new Scene(root);
-		
-		stage.setScene(scene);
 		stage.show();
+		populateElementToChoose(root);
 	}
 
-	/**
-	 * @param args the command line arguments
-	 */
+	private void populateElementToChoose(Parent root) {
+		RowData splitter1 = new RowData("Rozdzielacz 1:2", 1, 0.2, 3.5);
+		RowData splitter4 = new RowData("Rozdzielacz 1:4", 1, 0.3, 6.7);
+		RowData splitter6 = new RowData("Rozdzielacz 1:6", 1, 0.3, 8.7);
+		RowData splitter8 = new RowData("Rozdzielacz 1:8", 1, 0.55, 9.8);
+		RowData splitter16 = new RowData("Rozdzielacz 1:16", 1, 0.67, 13.1);
+		RowData splitter32 = new RowData("Rozdzielacz 1:32", 1, 0.9, 17);
+		RowData splitter64 = new RowData("Rozdzielacz 1:64", 1, 1.2, 20.8);
+		RowData elementWDM = new RowData("Element WDM", 1, 0.1, 0.5);
+		RowData fibre = new RowData("Swiatlowod", 1, 0.1, 0.35);
+		RowData separableConnector = new RowData("zlacze rozlaczne", 1, 0.1, 0.4);
+		RowData weld = new RowData("spaw ", 1, 0.05, 0.1);
+
+		ComboBox elementList = (ComboBox) root.lookup("#elementsCBox");
+		ObservableList<RowData> data = FXCollections.observableArrayList();
+		data.addAll(splitter1, splitter4, splitter6, splitter8, splitter16, splitter32, splitter64,
+				elementWDM, fibre, separableConnector, weld);
+		elementList.setItems(data);
+
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 }
---
+++ src/topt/RowData.java
@@ -0,0 +1,70 @@
+package topt;
+
+import javafx.beans.property.*;
+
+public class RowData {
+    private StringProperty name;
+    private IntegerProperty nrOfElements;
+    private DoubleProperty standardDev;
+    private DoubleProperty expectedValue;
+
+    private StringProperty nameProperty() {
+        if (name == null) name = new SimpleStringProperty(this, "name");
+        return name;
+    }
+    private IntegerProperty nrOfElementsProperty() {
+        if (nrOfElements == null) nrOfElements = new SimpleIntegerProperty(this, "nrOfElements");
+        return nrOfElements;
+    }
+    private DoubleProperty standardDevProperty() {
+        if (standardDev == null) standardDev = new SimpleDoubleProperty(this, "standardDev");
+        return standardDev ;
+    }
+    private DoubleProperty expectedValueProperty() {
+        if (expectedValue == null) expectedValue = new SimpleDoubleProperty(this, "expectedValue");
+        return expectedValue;
+    }
+    public RowData(String name,int nrOfElements,double standardDev,double expectedValue) {
+       nameProperty().setValue(name);
+       nrOfElementsProperty().setValue(nrOfElements);
+       standardDevProperty().setValue(standardDev);
+       expectedValueProperty().setValue(expectedValue);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public int getNrOfElements() {
+        return nrOfElements.get();
+    }
+
+    public void setNrOfElements(int nrOfElements) {
+        this.nrOfElements.set(nrOfElements);
+    }
+
+    public double getStandardDev() {
+        return standardDev.get();
+    }
+
+    public void setStandardDev(double standardDev) {
+        this.standardDev.set(standardDev);
+    }
+
+    public double getExpectedValue() {
+        return expectedValue.get();
+    }
+
+    public void setExpectedValue(double expectedValue) {
+        this.expectedValue.set(expectedValue);
+    }
+    public String toString(){
+        return name.getValue();
+
+
+    }
+}
